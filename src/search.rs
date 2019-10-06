c2edd3e537dada7ebeea12cc7ef42306921c2a93
Jannis Harder <me@jix.one>
2019-10-06 10:34:37 +0200

    Refactor search

--- src/search.rs
+++ src/search.rs
@@ -1,8 +1,11 @@
 use std::{
     cmp::Reverse,
     collections::BinaryHeap,
+    rc::Rc,
     time::{Duration, Instant},
 };
+
+use rustc_hash::FxHashMap as HashMap;
 
 mod states;
 
@@ -36,6 +39,7 @@ impl Search {
             }
             state = search.improve(initial.as_ref());
             log::info!("bounds: {:?}", state);
+            search.log_stats();
         }
     }
 
@@ -117,41 +121,27 @@ impl Search {
             state = new_state;
         }
 
-        let mut successors = vec![];
+        let mut edges = Edges::default();
 
         for i in 0..output_set.channels() {
             for j in 0..i {
-                let mut successor = output_set.to_owned();
-                if successor.apply_comparator([i, j]) {
-                    successor.canonicalize(true);
-                    successors.push(successor);
+                let mut target = output_set.to_owned();
+                if target.apply_comparator([i, j]) {
+                    target.canonicalize(true);
+
+                    edges.add_edge(self, target);
                 }
             }
         }
 
-        successors.sort_unstable();
-        successors.dedup();
-
         loop {
-            successors.sort_by_cached_key(|successor| {
-                let successor_state = self.states.get(successor.as_ref());
-                (successor_state.bounds[0], successor.len())
-            });
+            let combined_upper_bound =
+                edges.states().map(|state| state.bounds[1]).min().unwrap() + 1;
 
-            let successor_combined_upper_bound = successors
-                .iter()
-                .map(|successor| {
-                    let successor_state = self.states.get(successor.as_ref());
-                    successor_state.bounds[1]
-                })
-                .min()
-                .unwrap()
-                + 1;
-
-            if successor_combined_upper_bound < state.bounds[1] {
-                state.bounds[1] = successor_combined_upper_bound;
-                if successor_combined_upper_bound < state.huffman_bounds[1] {
-                    state.huffman_bounds[1] = successor_combined_upper_bound;
+            if combined_upper_bound < state.bounds[1] {
+                state.bounds[1] = combined_upper_bound;
+                if combined_upper_bound < state.huffman_bounds[1] {
+                    state.huffman_bounds[1] = combined_upper_bound;
                 }
                 self.states.set(output_set, state);
                 if state.bounds[0] == state.bounds[1] {
@@ -159,38 +149,18 @@ impl Search {
                 }
             }
 
-            let successor_combined_lower_bound = successors
-                .iter()
-                .map(|successor| {
-                    let successor_state = self.states.get(successor.as_ref());
-                    successor_state.bounds[0]
-                })
-                .min()
-                .unwrap()
-                + 1;
+            let combined_lower_bound =
+                edges.states().map(|state| state.bounds[0]).min().unwrap() + 1;
 
-            if successor_combined_lower_bound > state.bounds[0] {
+            if combined_lower_bound > state.bounds[0] {
                 let mut state = self.states.get(output_set);
-                state.bounds[0] = successor_combined_lower_bound;
+                state.bounds[0] = combined_lower_bound;
                 self.states.set(output_set, state);
 
                 return state;
             }
 
-            for successor in successors.iter() {
-                let mut successor_state = self.states.get(successor.as_ref());
-                let mut explored = false;
-                while successor_state.bounds[0] != successor_state.bounds[1]
-                    && successor_state.bounds[0] + 1 <= state.bounds[0]
-                {
-                    successor_state = self.improve(successor.as_ref());
-                    explored = true;
-                }
-
-                if explored {
-                    break;
-                }
-            }
+            edges.improve_next(self);
         }
     }
 
@@ -211,46 +181,46 @@ impl Search {
             }
         }
 
-        let mut pruned_output_sets = [vec![], vec![]];
+        let mut edges = Edges::default();
 
-        for (pol, (pol_channels, pol_output_sets)) in extremal_channels
+        let mut pruned_ids = [vec![], vec![]];
+
+        for (pol, (pol_channels, pol_ids)) in extremal_channels
             .iter()
-            .zip(pruned_output_sets.iter_mut())
+            .zip(pruned_ids.iter_mut())
             .enumerate()
         {
             for &channel in pol_channels.iter() {
-                let mut pruned_output_set = OutputSet::all_values(output_set.channels() - 1);
-                output_set.prune_extremal_channel_into(
-                    pol > 0,
-                    channel,
-                    pruned_output_set.as_mut(),
-                );
-                pruned_output_set.canonicalize(true);
+                let mut target = OutputSet::all_values(output_set.channels() - 1);
+                output_set.prune_extremal_channel_into(pol > 0, channel, target.as_mut());
+                target.canonicalize(true);
 
-                pol_output_sets.push((
-                    self.states.get(pruned_output_set.as_ref()),
-                    pruned_output_set.len(),
-                    pruned_output_set,
-                ));
+                pol_ids.push(edges.add_edge(self, target));
             }
         }
 
         let mut upper_huffman_bounds = [state.huffman_bounds[1], state.huffman_bounds[1]];
 
         while state.huffman_bounds[1] > state.bounds[0] {
-            for (pol, pol_output_sets) in pruned_output_sets.iter().enumerate() {
+            for (pol, pol_ids) in pruned_ids.iter().enumerate() {
                 if upper_huffman_bounds[pol] > state.bounds[0] {
-                    let lower_bounds = pol_output_sets
+                    let lower_bounds = pol_ids
                         .iter()
-                        .map(|(pruned_state, _, _)| pruned_state.bounds[0])
+                        .map(|&id| edges.state(id).bounds[0])
                         .collect::<CVec<_>>();
-                    let upper_bounds = pol_output_sets
+                    let upper_bounds = pol_ids
                         .iter()
-                        .map(|(pruned_state, _, _)| pruned_state.bounds[1])
+                        .map(|&id| edges.state(id).bounds[1])
                         .collect::<CVec<_>>();
 
                     let lower_huffman_bound = max_plus_1_huffman(&lower_bounds);
                     let upper_huffman_bound = max_plus_1_huffman(&upper_bounds);
+
+                    if upper_huffman_bound <= state.bounds[0] {
+                        edges
+                            .active_ids
+                            .retain(|id| pruned_ids[1 - pol].contains(id));
+                    }
 
                     upper_huffman_bounds[pol] = upper_huffman_bound;
 
@@ -273,26 +243,73 @@ impl Search {
                 self.states.set(output_set, state);
             }
 
-            if let Some((cached_pruned_state, _len, pruned_output_set)) = pruned_output_sets
-                .iter_mut()
-                .zip(upper_huffman_bounds.iter())
-                .filter(|(_, upper_bound)| **upper_bound > state.bounds[0])
-                .flat_map(|(pol_output_sets, _)| pol_output_sets.iter_mut())
-                .filter(|(pruned_state, _, _)| pruned_state.bounds[0] != pruned_state.bounds[1])
-                .min_by_key(|(pruned_state, len, _)| (pruned_state.bounds[0], *len))
-            {
-                let pruned_state = self.states.get(pruned_output_set.as_ref());
-                if pruned_state != *cached_pruned_state {
-                    *cached_pruned_state = pruned_state;
-                } else {
-                    *cached_pruned_state = self.improve(pruned_output_set.as_ref());
-                }
-            } else {
-                break;
-            }
+            edges.improve_next(self);
         }
 
         state
+    }
+}
+
+#[derive(Default)]
+struct Edges {
+    target_to_id: HashMap<Rc<OutputSet>, usize>,
+    targets: Vec<(Rc<OutputSet>, State, usize)>,
+    active_ids: Vec<usize>,
+}
+
+impl Edges {
+    fn add_edge(&mut self, search: &Search, target: OutputSet) -> usize {
+        let target = Rc::new(target);
+
+        let targets = &mut self.targets;
+        let active_ids = &mut self.active_ids;
+        *self.target_to_id.entry(target.clone()).or_insert_with(|| {
+            let id = targets.len();
+            let state = search.states.get(target.as_ref().as_ref());
+            let len = target.len();
+            targets.push((target, state, len));
+            if state.bounds[0] != state.bounds[1] {
+                active_ids.push(id);
+            }
+            id
+        })
+    }
+
+    fn states<'a>(&'a self) -> impl Iterator<Item = State> + 'a {
+        self.targets.iter().map(|(_, state, _)| *state)
+    }
+
+    fn state(&self, id: usize) -> State {
+        let (_, state, _) = self.targets[id];
+        state
+    }
+
+    fn sort_key(&self, id: usize) -> (u8, usize, u8) {
+        let (_, state, len) = &self.targets[id];
+
+        (state.bounds[0], *len, state.bounds[1])
+    }
+
+    fn improve_next(&mut self, search: &mut Search) -> bool {
+        for i in (1..self.active_ids.len()).rev() {
+            if self.sort_key(self.active_ids[i]) < self.sort_key(self.active_ids[i - 1]) {
+                self.active_ids.swap(i, i - 1);
+            }
+        }
+
+        if self.active_ids.is_empty() {
+            false
+        } else {
+            // TODO allow work stealing
+            let id = self.active_ids[0];
+            let (output_set, state, _len) = &mut self.targets[id];
+            *state = search.improve(output_set.as_ref().as_ref());
+
+            if state.bounds[0] == state.bounds[1] {
+                self.active_ids.swap_remove(0);
+            }
+            true
+        }
     }
 }
 
